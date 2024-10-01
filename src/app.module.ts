6d70e07b2875d317759a93cbc7d603eea5580c10
Deivis Torres Mena <babyyoda62406@gmail.com>
2024-10-01 05:12:34 -0400

    config: DB-Connect

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,27 @@
 import { Module } from '@nestjs/common';
-
+import { APP_PIPE } from '@nestjs/core';
+import { CustomValidationPipe } from './common/Pipes/CustomValidationPipe';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { envs } from './config';
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: envs.DB_HOST,
+      port: Number(envs.DB_PORT),
+      username: envs.DB_USER,
+      password: envs.DB_PASS,
+      database: envs.DB_NAME,
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_PIPE,
+      useClass: CustomValidationPipe
+    }
+  ],
 })
-export class AppModule {}
+export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { envs } from './config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger  = new Logger("GPYP-API")
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.setGlobalPrefix('api/v1');
+
+  await app.listen(envs.PORT);
+  logger.log(`Api is running on port: ${envs.PORT}`)
 }
 bootstrap();
